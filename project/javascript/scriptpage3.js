cbd13b6b72dbd383b2591740520fbf326b7ad51e
Yana Demidenko <126800596+yana-demidenko@users.noreply.github.com>
2023-06-03 02:22:15 +0300

    Update scriptpage3.js

--- project/javascript/scriptpage3.js
+++ project/javascript/scriptpage3.js
@@ -1,3 +1,17 @@
+let btnMenu = document.querySelector('.meenu');
+let menu = document.querySelector('.background');
+let x = document.querySelector('.mobile-menu');
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('open');
+  menu.classList.toggle('open');
+})
+x.addEventListener('click', () => {
+  menu.classList.toggle('open');
+})
+document.querySelector('.meenu').addEventListener('click', function(event) {
+  event.preventDefault(); // предотвращаем перенаправление на верх страницы
+});
+
 const video = document.getElementById('video');
 
 function changeVideoSrc() {
