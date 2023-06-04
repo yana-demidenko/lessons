604c6a2312b3b05de37fc39749e54a4628aacbed
Yana Demidenko <126800596+yana-demidenko@users.noreply.github.com>
2023-06-04 19:08:10 +0300

    Update scriptpage4.js

--- project/javascript/scriptpage4.js
+++ project/javascript/scriptpage4.js
@@ -1,40 +1,26 @@
-let btnMenu = document.querySelector('.meenu');
-let menu = document.querySelector('.background');
-let x = document.querySelector('.mobile-menu');
-btnMenu.addEventListener('click', () => {
-  btnMenu.classList.toggle('open');
-  menu.classList.toggle('open');
-})
-x.addEventListener('click', () => {
-  menu.classList.toggle('open');
-})
-document.querySelector('.meenu').addEventListener('click', function(event) {
-  event.preventDefault(); // предотвращаем перенаправление на верх страницы
-});
-
 const image1 = document.getElementById('image1');
 
 function changeImageSrc() {
-    const screenSize = window.innerWidth;
+  const screenSize = window.innerWidth;
 
-    if (screenSize <= 1020) { // меняем src на маленькую картинку
-        image1.setAttribute('src', './image/page4-image-mobile.webp');
-    } else { // меняем src на большую картинку
-        image1.setAttribute('src', './image/page4-image-1.webp');
-    }
+  if (screenSize <= 1020) { // меняем src на маленькую картинку
+    image1.setAttribute('src', './image/page4-image-mobile.webp');
+  } else { // меняем src на большую картинку
+    image1.setAttribute('src', './image/page4-image-1.webp');
+  }
 }
 window.addEventListener('resize', changeImageSrc); // отслеживаем изменение размера экрана
 
 var swiper = new Swiper('.swiper', {
   slidesPerView: 4,
   spaceBetween: 5,
- 
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: { 
+  breakpoints: {
     240: {
       slidesPerView: 1,
       spaceBetween: 10
