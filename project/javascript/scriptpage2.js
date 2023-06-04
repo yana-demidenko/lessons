5116830f5e91eb9b8e34fec42ec6942f6a7fad98
Yana Demidenko <126800596+yana-demidenko@users.noreply.github.com>
2023-06-04 19:08:00 +0300

    Update scriptpage2.js

--- project/javascript/scriptpage2.js
+++ project/javascript/scriptpage2.js
@@ -1,17 +1,3 @@
-let btnMenu = document.querySelector('.meenu');
-let menu = document.querySelector('.background');
-let x = document.querySelector('.mobile-menu');
-btnMenu.addEventListener('click', () => {
-  btnMenu.classList.toggle('open');
-  menu.classList.toggle('open');
-})
-x.addEventListener('click', () => {
-  menu.classList.toggle('open');
-})
-document.querySelector('.meenu').addEventListener('click', function(event) {
-  event.preventDefault(); // предотвращаем перенаправление на верх страницы
-});
-
 const image = document.getElementById('image1');
 
 function changeImageSrc() {
