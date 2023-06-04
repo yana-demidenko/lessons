f3c4655f63d60a01ea912df87fb4fed907b8c920
Yana Demidenko <126800596+yana-demidenko@users.noreply.github.com>
2023-06-04 04:18:39 +0300

    Update scriptpage4.js

--- project/javascript/scriptpage4.js
+++ project/javascript/scriptpage4.js
@@ -24,3 +24,32 @@ function changeImageSrc() {
     }
 }
 window.addEventListener('resize', changeImageSrc); // отслеживаем изменение размера экрана
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 5,
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: { 
+    240: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+    550: {
+      slidesPerView: 2,
+      spaceBetween: 5
+    },
+    830: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    1145: {
+      slidesPerView: 4,
+      spaceBetween: 5
+    },
+  }
+});
